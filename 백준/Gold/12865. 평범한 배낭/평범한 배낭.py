52e13dab57060c063f53671a1eb93b2f8467558d
OH Yunju <84166982+OH-YUNJU@users.noreply.github.com>
2023-03-03 17:02:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 5576 ms, Memory: 229220 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+weight = [[0, 0]]
+dp = [[0]*(k+1) for _ in range(n+1)]
+for i in range(n):
+    weight.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = weight[i][0]
+        v = weight[i][1]
+        
+        if(j < w):
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+print(dp[n][k])
