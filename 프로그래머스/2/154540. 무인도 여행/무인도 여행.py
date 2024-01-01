ee468addd7199ae1bec3d27015340ecea5474858
OH Yunju <84166982+OH-YUNJU@users.noreply.github.com>
2024-01-01 16:44:23 +0900

    [level 2] Title: 무인도 여행, Time: 0.20 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+dx, dy = [0,0,-1,1], [-1,1,0,0]
+
+def bfs(maps, visit, i, j, n, m):
+    queue = deque()
+    queue.append((i, j))
+    visit[i][j] = 1
+    
+    area = int(maps[i][j])
+    
+    while(queue):
+        x, y = queue.popleft()
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if(0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0 and maps[nx][ny] != 'X'):
+                visit[nx][ny] = 1
+                area += int(maps[nx][ny])
+                queue.append((nx, ny))
+    return area
+            
+def solution(maps):
+    answer = []
+    n = len(maps)
+    m = len(maps[0])
+    visit = [[0]*m for i in range(n)]
+    
+    for i in range(n):
+        for j in range(m):
+            if(maps[i][j] != 'X' and visit[i][j] == 0):
+                answer.append(bfs(maps, visit, i, j, n, m))
+    
+    if(answer):
+        return sorted(answer)
+    else:
+        return [-1]
+    
+    return answer
