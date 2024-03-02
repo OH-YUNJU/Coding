5bfcbcdf159192876ca910cf8308708bb2f0ce84
OH Yunju <84166982+OH-YUNJU@users.noreply.github.com>
2024-03-03 06:10:25 +0900

    [Silver III] Title: 1로 만들기, Time: 524 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,14 @@
+# dp
+# 시간복잡도 n
+# 
 n = int(input())
-cnt = 0
-li = [0,0,1,1]
+dp = [0,0,1,1]
 
 for i in range(4, n+1):
-
-    li.append(li[i-1] + 1)
+    dp.append(dp[i-1] + 1)
     
-    if i % 2 == 0:
-        li[i] = min(li[i//2] + 1, li[i])
-    if i % 3 == 0:
-        li[i] = min(li[i//3] + 1, li[i])
-        
-print(li[n])
+    if(i%2 == 0):
+        dp[i] = min(dp[i//2]+1, dp[i])
+    if(i%3 == 0):
+        dp[i] = min(dp[i//3]+1, dp[i])
+print(dp[n])
